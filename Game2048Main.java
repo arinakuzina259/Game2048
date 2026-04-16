7d676c9f42ba6bb52a682be8490a73530e3cb3f6
arinakuzina259 <133975882+arinakuzina259@users.noreply.github.com>
2026-04-16 13:36:53 +0700

    Create Game2048Main.java

---
+++ Game2048Main.java
@@ -0,0 +1,23 @@
+package com.minesweeper;
+
+import javax.swing.*;
+
+/**
+ * Главный класс приложения. Точка входа в игру 2048.
+ */
+public class Game2048Main {
+    public static void main(String[] args) {
+        // Установка Look and Feel для улучшения внешнего вида на разных платформах
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        // Запуск приложения в потоке обработки событий Swing
+        SwingUtilities.invokeLater(() -> {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.setVisible(true);
+        });
+    }
+}
